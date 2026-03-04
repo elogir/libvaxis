6143b53f5756e9a50dfd2291172dfce26adb10df
Raphael NAHOUM <raphael@zml.ai>
2026-03-04 15:07:38 +0100

    Make PadValues public and add Image to vxfw

--- src/vxfw/vxfw.zig
+++ src/vxfw/vxfw.zig
@@ -16,6 +16,7 @@ pub const Button = @import("Button.zig");
 pub const Center = @import("Center.zig");
 pub const FlexColumn = @import("FlexColumn.zig");
 pub const FlexRow = @import("FlexRow.zig");
+pub const Image = @import("Image.zig");
 pub const ListView = @import("ListView.zig");
 pub const Padding = @import("Padding.zig");
 pub const RichText = @import("RichText.zig");
